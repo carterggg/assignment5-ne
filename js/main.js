00f7c6eb47d6d890a0e4ab451ef65d5210f48f71
Carter G <carterguthrie94@yahoo.com>
2014-12-03 04:07:02 -0500

    updated toggle just saying

--- js/main.js
+++ js/main.js
@@ -1,43 +1,5 @@
 
 
-//toggle effect
-
-// $('.toggle').click(function(){
-//	$(this).next().fadeToggle('slow');
-//	$(".toggle-info").toggleClass("hidden");
-
-// });
-
-
-
-
-$( ".toggle" ).click(function() {
-  $( ".toggle-info" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-});
-
-
-
-
-
-
-$('.toggle').click(function(){
-	$(this).next().fadeToggle('slow');
-	$(".toggle-info").toggleClass("hidden");
-
-});
-
-
-
-$( ".toggle" ).click(function() {
-  $( ".toggle-info" ).toggle( "slow" );
-});
-
-
-
-var flip = 0;
-$( ".toggle" ).click(function() {
-  $( ".toggle-info" ).toggle( flip++ % 2 === 0 );
-	$(".toggle-info").toggleClass("hidden");
+$(".toggle").click(function () {
+	$(".toggle-info").slideToggle("slow");
 });
--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,1 @@
-$(".toggle").click(function(){$(".toggle-info").toggle("slow",function(){})}),$(".toggle").click(function(){$(this).next().fadeToggle("slow"),$(".toggle-info").toggleClass("hidden")}),$(".toggle").click(function(){$(".toggle-info").toggle("slow")});var flip=0;$(".toggle").click(function(){$(".toggle-info").toggle(flip++%2===0),$(".toggle-info").toggleClass("hidden")});
+$(".toggle").click(function(){$(".toggle-info").slideToggle("slow")});

e0090c098dd077e8c57f36bb78e592164784fd7f
Carter G <carterguthrie94@yahoo.com>
2014-12-12 05:07:50 -0500

    updated

--- inc/mainnav.inc.php
+++ inc/mainnav.inc.php
@@ -1,9 +1,9 @@
 			<nav class="fixed-nav">
 				<ul>
-				 <li class="main-nav"><a href="news.html">NEWS</a></li>
-					<li class="main-nav"><a href="designs.html">DESIGNS</a></li>	
-					<li class="main-nav"><a href="about.html">AB0UT</a></li>	
-					<li class="main-nav"><a href="http://nothingextravagant.bigcartel.com/" target="_blank">SH0P</a></li>
-					<li class="main-nav"><a href="contact.php">C0NTACT</a></li>
+				 <li class="main-nav nav-a"><a href="news.html">NEWS</a></li>
+					<li class="main-nav nav-a"><a href="designs.html">DESIGNS</a></li>	
+					<li class="main-nav nav-a"><a href="about.html">AB0UT</a></li>	
+					<li class="main-nav nav-a"><a href="http://nothingextravagant.bigcartel.com/" target="_blank">SH0P</a></li>
+					<li class="main-nav nav-a"><a href="contact.php">C0NTACT</a></li>
 				</ul>
 			</nav>
